54d0d58b082cb45cf83b1b7401a4aad8f6f3fd10
李智 <2276923678@qq.com>
2022-07-26 17:26:35 +0800

    22

---
+++ src/views/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout">我是默认页面</div>
+</template>
+
+<script>
+export default {
+  name: "layout",
+  data: () => ({
+    index: 111,
+  }),
+};
+</script>
+
+<style>
+</style>
